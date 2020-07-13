8e2390e72647d36b25ecd3ad290e4f5cc99224b5
Steven Frasica <sfrasica1@gmail.com>
2020-07-12 23:37:36 -0400

    circle component

---
+++ phaser-practice/src/CircleComponent.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function CircleComponent() {
+    
+    let circle1 = this.add.circle(50, 100, 90, 0xFFF070);
+  let circle2 = this.add.circle(95, 300, 80, 0xFF0000);
+  let circle3 = this.add.circle(200, 200, 100, 0x9F00D0);
+  let circle4 = this.add.circle(300, 400, 10, 0x00E030);
+    
+  const config = {
+	type: Phaser.AUTO,
+	width: 450,
+	height: 600,
+	scene: {
+    create
+	}
+}
+
+const game = new Phaser.Game(config)
+    
+    
+    return (
+        <div>
+           {game} 
+        </div>
+    )
+}
